af13ac601160c82fa92bf14ffc5c853ec95db93d
nafisah <nafisahisbarsani12@gmail.com>
2025-10-16 09:32:59 +0700

    [NI]: game list

--- lib/features/games/games_page.dart
+++ lib/features/games/games_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:game_list_cubit/widgets/app_styles.dart';
+import 'package:game_list_cubit/widgets/custom_search_bar.dart';
 import '../../widgets/custom_card.dart';
 import 'cubit/games_cubit.dart';
 import 'cubit/games_state.dart';
@@ -37,7 +38,6 @@ class _GamesPageState extends State<GamesPage> {
   }
 
   void _onSearchChanged(String query) {
-    // Cancel previous timer
     if (_debounceTimer?.isActive ?? false) {
       _debounceTimer?.cancel();
     }
@@ -77,7 +77,6 @@ class _GamesPageState extends State<GamesPage> {
       ),
       body: Column(
         children: [
-          // SEARCH BAR DENGAN TEXT FIELD LANGSUNG
           _buildSearchBar(),
           Expanded(
             child: BlocBuilder<GameCubit, GameState>(
@@ -93,63 +92,16 @@ class _GamesPageState extends State<GamesPage> {
 
   Widget _buildSearchBar() {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Container(
-        width: 352,
-        height: 51,
-        clipBehavior: Clip.antiAlias,
-        decoration: ShapeDecoration(
-          color: AppStyles.black500,
-          shape: RoundedRectangleBorder(
-            side: BorderSide(
-              width: 3,
-              color: AppStyles.primary1,
-            ),
-            borderRadius: BorderRadius.circular(AppStyles.radiusXXXL),
-          ),
-        ),
-        child: Row(
-          children: [
-            Expanded(
-              child: TextField(
-                controller: _searchController,
-                style: AppStyles.xsmedium(color: AppStyles.black00),
-                decoration: InputDecoration(
-                  hintText: 'Search games...',
-                  hintStyle: AppStyles.xsmedium(color: AppStyles.black00.withOpacity(0.5)),
-                  border: InputBorder.none,
-                  contentPadding: const EdgeInsets.only(left: 21, top: 16),
-                  isDense: true,
-                ),
-                onChanged: _onSearchChanged,
-              ),
-            ),
-            // Search Icon atau Clear Icon
-            if (_searchController.text.isNotEmpty)
-              IconButton(
-                icon: Icon(
-                  Icons.clear,
-                  color: AppStyles.primary1,
-                  size: AppStyles.iconL,
-                ),
-                onPressed: _clearSearch,
-              )
-            else
-              Padding(
-                padding: const EdgeInsets.only(right: 16),
-                child: Icon(
-                  Icons.search,
-                  color: AppStyles.primary1,
-                  size: AppStyles.iconL,
-                ),
-              ),
-          ],
-        ),
+      padding: const EdgeInsets.all(AppStyles.paddingXXL),
+      child: CustomSearchBar(
+        controller: _searchController,
+        onChanged: _onSearchChanged,
+        onClear: _clearSearch,
       ),
     );
   }
 
-  Widget _buildBody(GameState state) {
+  Widget _buildBody(GameState state){
     return switch (state) {
       GameInitial() => Center(
         child: Text(
@@ -157,11 +109,13 @@ class _GamesPageState extends State<GamesPage> {
           style: AppStyles.smedium(color: AppStyles.black00),
         ),
       ),
+
       GameLoading() => const Center(
         child: CircularProgressIndicator(
           color: Color(0xFFFFC3DD),
         ),
       ),
+
       GameSuccess() => state.games.isEmpty
           ? Center(
         child: Column(
@@ -180,7 +134,8 @@ class _GamesPageState extends State<GamesPage> {
             SizedBox(height: AppStyles.spaceS),
             Text(
               'Try a different search term',
-              style: AppStyles.smedium(color: AppStyles.black00.withOpacity(0.7)),
+              style: AppStyles.smedium(
+                  color: AppStyles.black00.withOpacity(0.7)),
             ),
           ],
         ),
@@ -192,6 +147,7 @@ class _GamesPageState extends State<GamesPage> {
           return CustomCard(game: game);
         },
       ),
+
       GameError() => Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -223,6 +179,7 @@ class _GamesPageState extends State<GamesPage> {
           ],
         ),
       ),
+
       _ => Center(
         child: Text(
           'Unknown state',
